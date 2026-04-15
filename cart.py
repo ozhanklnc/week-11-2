26a038ff9e08354987db7494daf41fa7db981a5a
Özhan Kılınç <ozhanklnc@gmail.com>
2026-04-15 11:05:29 +0300

    Update cart.py

--- cart.py
+++ cart.py
@@ -88,7 +88,7 @@ class ShoppingCart:
 
         This method is not implemented yet.
         """
-        raise NotImplementedError("get_item_count() is not implemented yet.")
+        return sum(item["quantity"] for item in self._items.values())
 
     def _subtotal(self) -> float:
         return sum(
